7f8dd5b11ea2d63da545cdf6ed568f6c73b69ddf
Diana Ivakhnenko <dirostiv@gmail.com>
2025-09-29 16:34:43 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,26 @@
+
+int main() {
+    int N, C, K;
+    cout << "Enter N: ";
+    cin >> N;
+    cout << "Enter C: ";
+    cin >> C;
+    cout << "Enter K: ";
+    cin >> K;
+
+    vector<int> gains(K), prices(K);
+
+    cout << "Enter gains array:\n";
+    for (int i = 0; i < K; ++i)
+        cin >> gains[i];
+
+    cout << "Enter prices array:\n";
+    for (int i = 0; i < K; ++i) 
+        cin >> prices[i];
+
+    int result = maximizeCapital(N, C, gains, prices);
+
+    cout << "Final capital after summer: " << result << endl;
+
+    return 0;
+}
